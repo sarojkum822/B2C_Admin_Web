9d35641d27be692e69aa673737b028a3bdaa6070
Sameersingh29 <sameerchandra12@gmail.com>
2024-09-30 23:14:02 +0530

    Navbar

---
+++ B2C-Admin-Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+import SideBar from './components/Leftsidebar'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <SideBar/>
+    </>
+  )
+}
+
+export default App
