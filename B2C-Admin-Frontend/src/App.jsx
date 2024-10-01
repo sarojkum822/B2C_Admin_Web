0e5cb0d753e524f745dbb775b833e67c9ca34817
Sameersingh29 <sameerchandra12@gmail.com>
2024-10-01 13:45:50 +0530

    Login Page Added

--- B2C-Admin-Frontend/src/App.jsx
+++ B2C-Admin-Frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import react from 'react'
-import SideBar from './components/Leftsidebar'
-
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SideBar from './components/Leftsidebar';
+import Login from './components/login';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-    <SideBar/>
-    </>
-  )
+    <Router>
+      <Routes>
+        
+        <Route path="/login" element={<Login />} />
+
+        
+        <Route path="/" element={<SideBar />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
